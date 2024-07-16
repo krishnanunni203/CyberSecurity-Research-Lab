d35f82b386a0f1f69400fb9eb46eac6d515df16a
krishnanunni203 <82704453+krishnanunni203@users.noreply.github.com>
2024-07-16 18:54:11 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     const sections = document.querySelectorAll('.section');
-    const navLinks = document.querySelectorAll('.nav-link');
+    const navLinks = document.querySelectorAll('.icon-navbar .nav-link');
 
     navLinks.forEach(link => {
         link.addEventListener('click', function(event) {
@@ -27,27 +27,5 @@ document.addEventListener('DOMContentLoaded', () => {
         if (section.classList.contains('home-section')) {
             section.classList.add('active');
         }
-    });
-
-    // Handle sub-navbar links
-    const subNavLinks = document.querySelectorAll('.sub-navbar .nav-link');
-    subNavLinks.forEach(link => {
-        link.addEventListener('click', function(event) {
-            event.preventDefault(); // Prevent default action of anchor tag
-
-            const targetId = this.getAttribute('href').substring(1);
-            sections.forEach(section => {
-                section.classList.remove('active');
-                if (section.id === targetId) {
-                    section.classList.add('active');
-                    section.scrollIntoView({ behavior: 'smooth', block: 'start' });
-                }
-            });
-
-            subNavLinks.forEach(navLink => {
-                navLink.classList.remove('active');
-            });
-            this.classList.add('active');
-        });
     });
 });
