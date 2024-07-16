35fb657cd048d421cacf1ee514ea46d2f7e1e39d
krishnanunni203 <82704453+krishnanunni203@users.noreply.github.com>
2024-07-16 10:47:39 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,22 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const links = document.querySelectorAll('.nav-link');
     const sections = document.querySelectorAll('.section');
-    const navLinks = document.querySelectorAll('.nav-link');
 
-    navLinks.forEach(link => {
-        link.addEventListener('click', function() {
-            const targetId = this.getAttribute('href').substring(1);
-            sections.forEach(section => {
-                if (section.id === targetId) {
-                    section.classList.add('active');
-                } else {
-                    section.classList.remove('active');
-                }
-            });
+    function showSection(id) {
+        sections.forEach(section => {
+            section.classList.remove('active');
+        });
+        document.querySelector(id).classList.add('active');
+    }
 
-            navLinks.forEach(navLink => {
-                navLink.classList.remove('active');
-            });
-            this.classList.add('active');
+    links.forEach(link => {
+        link.addEventListener('click', (e) => {
+            e.preventDefault();
+            const targetId = e.target.getAttribute('href');
+            showSection(targetId);
         });
     });
 });
